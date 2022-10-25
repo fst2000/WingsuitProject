bb12b5453dbdcd8fb4d3ac1912b9474f659986fb
fst2000 <fst.18.work@gmail.com>
2022-10-25 20:16:24 +0300

    animator blend tree added

--- Assets/Scripts/WingsuitController.cs
+++ Assets/Scripts/WingsuitController.cs
@@ -9,20 +9,23 @@ public class WingsuitController : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] Transform pivotPosition;
     new Rigidbody rigidbody;
+    TrailRenderer trailRenderer;
     [SerializeField] float backWingRotationAngle = 15f;
     [SerializeField] float forwardWingRotationAngle = 3f;
     [SerializeField] float dragForce = 10f;
+    [SerializeField] float angularDrag = 2f;
     PlayerInput playerInput = new PlayerInput();
 
-    Wing wingLeft = new Wing(Vector3.back, new Vector3(-0.5f, 1, 0), 1f, 0.5f);
-    Wing wingRight = new Wing(Vector3.back, new Vector3(0.5f, 1, 0), 1f, 0.5f);
-    Wing wingBack = new Wing(Vector3.back, new Vector3(0, 0, 0), 0.5f, 1f);
+    Wing wingLeft = new Wing(Vector3.ClampMagnitude(new Vector3(3,-3,-10),1), new Vector3(-0.5f, 1, 0), 0.5f, 1f);
+    Wing wingRight = new Wing(Vector3.ClampMagnitude(new Vector3(-3, -3, -10), 1), new Vector3(0.5f, 1, 0), 0.5f, 1f);
+    Wing wingBack = new Wing(Vector3.ClampMagnitude(new Vector3(0, -3, -10), 1), new Vector3(0, 0, 0), 0.5f, 1f);
     Wing wingStabilize = new Wing(Vector3.right, new Vector3(0, 0.2f, 0), 0.3f, 1f);
     Wing[] wings;
     private void Start()
     {
         rigidbody = gameObject.AddComponent<Rigidbody>();
         rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rigidbody.angularDrag = angularDrag;
         rigidbody.mass = 60;
         wings = new Wing[] { wingLeft, wingRight , wingBack, wingStabilize };
         transform.rotation = Quaternion.Euler(90,0,0);
@@ -39,6 +42,8 @@ public class WingsuitController : MonoBehaviour
     private void Update()
     {
         animator.SetBool("isFlying", true);
+        animator.SetFloat("flyVertical", -playerInput.moveVertical);
+        animator.SetFloat("flyHorizontal", playerInput.moveHorizontal);
         wingLeft.UpdateRotation(new Vector3(-playerInput.moveHorizontal * forwardWingRotationAngle, 0, 0));
         wingRight.UpdateRotation(new Vector3(playerInput.moveHorizontal * forwardWingRotationAngle, 0, 0));
         wingBack.UpdateRotation(new Vector3(-playerInput.moveVertical * backWingRotationAngle, 0, 0));
