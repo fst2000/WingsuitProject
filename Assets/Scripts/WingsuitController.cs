f36f042f477274f9f63a84c85fbe5cf134490a62
fst2000 <fst.18.work@gmail.com>
2022-10-26 20:50:40 +0300

    wings shaking partial added

--- Assets/Scripts/WingsuitController.cs
+++ Assets/Scripts/WingsuitController.cs
@@ -14,15 +14,24 @@ public class WingsuitController : MonoBehaviour
     [SerializeField] float forwardWingRotationAngle = 3f;
     [SerializeField] float dragForce = 10f;
     [SerializeField] float angularDrag = 2f;
+    [SerializeField] float boneRotateIntencity = 0.05f;
+    [SerializeField] float boneRotateSpeed = 15f;
+    [SerializeField] Transform boneArmL;
+    [SerializeField] Transform boneArmR;
+    [SerializeField] Transform boneHipL;
+    [SerializeField] Transform boneHipR;
+    Transform[] rotatedBones;
     PlayerInput playerInput = new PlayerInput();
 
-    Wing wingLeft = new Wing(Vector3.ClampMagnitude(new Vector3(3,-3,-10),1), new Vector3(-0.5f, 1, 0), 0.5f, 1f);
-    Wing wingRight = new Wing(Vector3.ClampMagnitude(new Vector3(-3, -3, -10), 1), new Vector3(0.5f, 1, 0), 0.5f, 1f);
+    Wing wingLeft = new Wing(Vector3.ClampMagnitude(new Vector3(3,-3,-10),1), new Vector3(-0.5f, 1, 0), 1f, 1f);
+    Wing wingRight = new Wing(Vector3.ClampMagnitude(new Vector3(-3, -3, -10), 1), new Vector3(0.5f, 1, 0), 1f, 1f);
     Wing wingBack = new Wing(Vector3.ClampMagnitude(new Vector3(0, -3, -10), 1), new Vector3(0, 0, 0), 0.5f, 1f);
     Wing wingStabilize = new Wing(Vector3.right, new Vector3(0, 0.2f, 0), 0.3f, 1f);
     Wing[] wings;
     private void Start()
     {
+        rotatedBones = new Transform[] {boneArmL,boneArmR,boneHipL,boneHipR };
+
         rigidbody = gameObject.AddComponent<Rigidbody>();
         rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         rigidbody.angularDrag = angularDrag;
@@ -56,8 +65,15 @@ public class WingsuitController : MonoBehaviour
             Vector3 globalVelocity = rigidbody.GetPointVelocity(wingGlobalPosition);
             rigidbody.AddForceAtPosition(globalNormal * Vector3.Dot(globalNormal, -globalVelocity) * wing.GetSquare() * dragForce, wingGlobalPosition);
         }
-
-
-
+    }
+    void LateUpdate()
+    {
+        float velocityMagnitude = Vector3.Magnitude(rigidbody.velocity);
+        foreach (Transform bone in rotatedBones)
+        {
+            Quaternion rotation = bone.localRotation;
+            float rotateAngle = Mathf.PerlinNoise(bone.localPosition.x * velocityMagnitude * boneRotateSpeed, bone.localPosition.y * velocityMagnitude * boneRotateSpeed) * boneRotateIntencity;
+            bone.localRotation = Quaternion.Euler(new Vector3(rotateAngle,0,0)) * rotation;
+        }
     }
 }
